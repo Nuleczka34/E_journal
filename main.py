ea17b2b0146d7db18ccf07c8e695673b2f23aef5
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2021-03-21 21:27:03 +0100

    Add a validate to semester after refactor

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Zaczynamy :)")
-print("XD")
+from studentfunction import add_student
+
+
+students_dict = {}
+flag = True
+index = 1
+
+# TODO: ZRÓB MENU GŁÓWNE
+while flag:
+    student_adding = add_student(students_dict, index)
+    if student_adding:
+        index += 1
+        print(students_dict)
--- studentfunction.py
+++ studentfunction.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from math import ceil
+
 
 students_dict = {}
-index = 0
 
 
 def date_valid(date: str) -> bool:
@@ -11,62 +12,105 @@ def date_valid(date: str) -> bool:
         age = actual_date.year - datetime_get.year - \
             ((actual_date.month, actual_date.day) < (datetime_get.month, datetime_get.day))
 
-        if 18 <= age <= 25:
-            return True
+        if 18 <= age:
+            return age
+
         else:
             print("\nStudent musi posiadać 18 lat!")
             return False
 
     except ValueError:
-        print("\nWpisana data jest niepoprawna Wzór:[dd-mm=yyyy]!")
+        print("\nWpisana data jest niepoprawna!\nWzór:[dd-mm-yyyy]")
         return False
 
 
-def name_valid(first_name):
-    if first_name.istitle() and 19 >= len(first_name) > 1:
+def name_valid(first_name: str) -> bool:
+    if first_name.istitle() and 1 < len(first_name) <= 19:
         return True
+
     else:
         print("\nImię(Imiona) studenta jest/są nieprawidłowe!\n"
               "Wzór:[Patryk / Patryk-Łukasz]")
         return False
 
 
-def surname_valid(student_surname):
-    if student_surname.istitle() and 30 >= len(student_surname) > 1:
+def surname_valid(student_surname: str) -> bool:
+    if student_surname.istitle() and 1 < len(student_surname) <= 30:
         return True
+
     else:
         print("\nNazwisko(Nazwiska) studenta jest/są nieprawidłowe!\n"
               "Wzór:[Kowalski/ Kowalski-Malinowski}")
         return False
 
 
-def semester_valid(sem):
-    sem = int(sem)
-    if 0 <= sem <= 12:
-        return True
+# TODO: POPRAW TEN MAKARON
+def semester_valid(sem: str, date_validate, when_birth: str) -> bool:
+    if sem.isnumeric():
+        sem = int(sem)
+        check_age = date_validate(when_birth)
+        actual_student_year = ceil(sem / 2)
+
+        if actual_student_year == 1 and check_age >= 18:
+            return True
+
+        elif actual_student_year == 2 and check_age >= 19:
+            return True
+
+        elif actual_student_year == 3 and check_age >= 20:
+            return True
+
+        elif actual_student_year == 4 and check_age >= 21:
+            return True
+
+        elif actual_student_year == 5 and check_age >= 22:
+            return True
+
+        elif actual_student_year == 6 and check_age >= 23:
+            return True
+
+        else:
+            print("\nLiczba semestru jest nieprawidłowa,wpisz liczbę z przedziału [1-12]\n"
+                  "Oraz sprawdź czy wiek studenta pozwala na umieszczenie go na danym semestrze")
+
+            return False
+
     else:
-        print("\nLiczba semestru jest nieprawidłowa,wpisz liczbę z przedziału [1-12]")
+        print("\nMusisz wpisać numer semestru!")
+
         return False
 
 
 def data_valid(name: str, surname: str, birthdate: str, semester: str) -> bool:
-    if name_valid(name) and surname_valid(surname) and date_valid(birthdate) and semester_valid(semester):
+
+    name_validate = name_valid(name)
+    surname_validate = surname_valid(surname)
+    date_validate = date_valid(birthdate)
+    semester_validate = semester_valid(semester, date_valid, birthdate)
+
+    if name_validate and surname_validate and date_validate and semester_validate:
         return True
+
     else:
         return False
 
 
-def add_student(gen_dict) -> dict:
-    global index
-    index += 1
+def add_student(gen_dict: dict, student_index: int) -> bool:
 
-    student_name = input("Podaj imię(imiona): ")
-    student_surname = input("Podaj nazwisko(nazwiska): ")
-    student_birthdate = input("Podaj datę urodzenia[dd.mm.yyyy]: ")
-    student_semester = input("Podaj obecny semestr: ")
+    student_name = input("Podaj imię(imiona): ").strip()
+    student_surname = input("Podaj nazwisko(nazwiska): ").strip()
+    student_birthdate = input("Podaj datę urodzenia[dd.mm.yyyy]: ").strip()
+    student_semester = input("Podaj obecny semestr: ").strip()
+
     if data_valid(student_name, student_surname, student_birthdate, student_semester):
         student_data_list = [student_name, student_surname, student_birthdate, student_semester]
-        gen_dict[index] = student_data_list
-        return gen_dict
+        gen_dict[student_index] = student_data_list
+
+        print("\nUdało się stworzyć studenta!")
+
+        return True
+
     else:
-        print("Nie udało się stworzyć studenta!")
+        print("\nNie udało się stworzyć studenta!")
+
+        return False
