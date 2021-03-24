a10d8db6c1a89b452e38d3d49db1145d500b06af
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2021-03-24 21:28:58 +0100

    Add display_student_dict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from student_add_function import json_decorator, student_add
-from student_rest_functions import display_student_dict
+from student_rest_functions import student_edit, display_student_dict
 from logging import basicConfig, DEBUG, exception
 
 basicConfig(filename="logging.log", level=DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
@@ -7,10 +7,10 @@ basicConfig(filename="logging.log", level=DEBUG, format='%(asctime)s:%(levelname
 
 # TODO: ZRÓB MENU GŁÓWNE
 @json_decorator
-def main(gen_dict: dict) -> dict:
-    index = gen_dict.get("index", 1)
-    if "index" in gen_dict:
-        gen_dict.pop("index")
+def main(students_dict: dict) -> dict:
+    index = students_dict.get("index", 1)
+    if "index" in students_dict:
+        students_dict.pop("index")
 
     flag = True
 
@@ -28,10 +28,10 @@ def main(gen_dict: dict) -> dict:
         chose_option = input("Wybierz numer operacji z listy: ").strip()
         try:
             if 0 < int(chose_option) <= 7:
-                menu_dict = {'1': ("Wyświetlanie listy", display_student_dict),  # TODO: WYŚWIETLANIE LISTY
+                menu_dict = {'1': ("Wyświetlanie listy", display_student_dict),
                              '2': ("Dodawanie studenta", student_add),
                              '3': ("Usuwanie studenta", "student_delete"),  # TODO: USUWANIE STUDENTA
-                             '4': ("Edycja studenta", "student_edit"),  # TODO: EDYTUJ DANE STUDENTA
+                             '4': ("Edycja studenta", student_edit),  # TODO: EDYTUJ DANE STUDENTA
                              '5': ("Wyświetlanie ocen", "display_student_note"),  # TODO: WYŚWIETLANIE OCEN
                              '6': ("Dodawanie ocen", """student_note_add"""),  # TODO: DODAWANIE OCEN
                              '7': ("Edycja ocen", "student_note_edit")}  # TODO: EDYCJA OCEN
@@ -40,16 +40,24 @@ def main(gen_dict: dict) -> dict:
 
                 try:
                     print(f"\nWybrałeś '{menu_result}'\n")
-                    if menu_result == "Dodawanie studenta":
+                    if menu_result == "Wyświetlanie listy":
+                        display_student_dict(students_dict)
+
+                    elif menu_result == "Dodawanie studenta":
                         student_name = input("Podaj imię(imiona): ").strip()
                         student_surname = input("Podaj nazwisko(nazwiska): ").strip()
                         student_birthdate = input("Podaj datę urodzenia[dd.mm.yyyy]: ").strip()
                         student_semester = input("Podaj obecny semestr: ").strip()
 
-                        adding_student = menu_chosen_func(gen_dict, index, student_name, student_surname,
+                        adding_student = menu_chosen_func(students_dict, index, student_name, student_surname,
                                                           student_birthdate, student_semester)
                         if adding_student:
                             index += 1
+                    elif menu_result == "Edycja studenta":
+                        pick_student_number = input("Podaj numer studenta, którego chcesz edytować: ")
+                        print(students_dict[pick_student_number])
+                        data_for_edit = input("Wybierz którą daną chcesz edytować: ") # TODO: DOKOŃCZ DEF I TUTAJ
+                        menu_chosen_func(students_dict, pick_student_number)
 
                 except Exception as func_except:
                     print("Wystąpił nieznany błąd")
@@ -64,8 +72,8 @@ def main(gen_dict: dict) -> dict:
         except ValueError:
             print("\nWybierz prawidłową operację!")
 
-    gen_dict["index"] = index
-    return gen_dict
+    students_dict["index"] = index
+    return students_dict
 
 
 if __name__ == '__main__':
--- student_rest_functions.py
+++ student_rest_functions.py
@@ -1,6 +1,11 @@
+def student_edit(gen_dict, chosen_student_number):
+    if chosen_student_number in gen_dict:
+        print(f"Wybrałeś: {gen_dict[chosen_student_number]}")
+    else:
+        print("Student o podanym numerze nie znajduje się na liście!")
+
+
 def display_student_dict(gen_dict):
-    try:
-        pick_student = int(input("Podaj numer studenta, którego chcesz edytować: "))
-    except ValueError:
-        print("Musisz wpisać numer")
-    print(f"Wybrałeś: {gen_dict[pick_student]}")
+    for student_number in gen_dict:
+        print(f"{student_number}. Imię: {gen_dict[student_number][0]}, Nazwisko: {gen_dict[student_number][1]}, "
+              f"Data Urodzenia: {gen_dict[student_number][2]}, Numer semestru: {gen_dict[student_number][3]}")
